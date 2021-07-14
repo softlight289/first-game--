2bd60035165ca5aebc585cbf2c59bfbee4e3d9b1
softlight289 <87424811+softlight289@users.noreply.github.com>
2021-07-14 19:34:53 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,76 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . a . . . 
+        a . . . . . . . . . . . a a a . 
+        a d d 5 d 5 d d d d d d a a a a 
+        a . . . . . . . . . . . a a a . 
+        . . . . . . . . . . . . a . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, spacePlane, 200, 0)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    scene.cameraShake(4, 500)
+    info.changeLifeBy(-1)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy(effects.fire, 500)
+    info.changeScoreBy(1)
+})
+let bogey: Sprite = null
+let projectile: Sprite = null
+let spacePlane: Sprite = null
+spacePlane = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . c . . . . . . . . . . 
+    . . . . . c c c . . . . . . . . 
+    . . . c . . c c c c . . . . . . 
+    . . . c c . c c c c c 9 9 . . . 
+    . . . c c c c c c c c c c c . . 
+    . . . . . . c c c c . . . . . . 
+    . . . . . . c c . . . . . . . . 
+    . . . . . c c . . . . . . . . . 
+    . . . . . c . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(spacePlane, 200, 200)
+spacePlane.setStayInScreen(true)
+info.setLife(3)
+game.onUpdateInterval(1000, function () {
+    bogey = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 8 8 8 . . . . . . 
+        . . . . . 6 6 6 6 6 6 6 . . . . 
+        . . . . . . 8 f 8 8 8 . . . . . 
+        9 6 6 9 6 6 9 6 6 6 9 6 6 . . . 
+        8 8 8 8 8 8 8 8 8 8 8 8 8 8 . . 
+        . 8 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+        . . 8 8 8 8 8 8 8 8 8 8 8 8 8 . 
+        . . . . . 8 8 8 8 8 8 8 8 8 8 . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+    bogey.setVelocity(-100, 0)
+    bogey.setPosition(160, randint(5, 115))
+    bogey.setFlag(SpriteFlag.AutoDestroy, true)
+})
